36393e3ed712ecd859ab8e7acb1f5abb8b55f4dc
pawel <plucinski@gmx.com>
2019-05-16 16:52:09 +0200

    parse petla

--- venv/owm.py
+++ venv/owm.py
@@ -1,5 +1,20 @@
 import requests
+import sys
+import time
 
-url = "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=23a8e5b6d7a5c5e91b7c93a703c196bb"
+city: object = sys.argv[1]
+interval = int(sys.argv[2])
+
+print(city)
+print(interval)
+
+url = "http://api.openweathermap.org/data/2.5/weather?q=%s&APPID=23a8e5b6d7a5c5e91b7c93a703c196bb" % city
+print(url)
 json_data = requests.get(url).json()
-print(json_data)
+#print(json_data)
+
+
+while(True):
+    json_data = requests.get(url).json()
+    print(json_data)
+    time.sleep(interval)
