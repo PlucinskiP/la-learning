dc0220f1ace034d4fcc8ba6f18c7546757b728e3
pawel <plucinski@gmx.com>
2019-05-20 12:24:22 +0200

    display humidity

--- venv/owm.py
+++ venv/owm.py
@@ -6,8 +6,8 @@ import time
 city = sys.argv[1]
 interval = int(sys.argv[2])
 
-print(city)
-print(interval)
+#print(city)
+#print(interval)
 
 def get_weather(loc):
     url = "http://api.openweathermap.org/data/2.5/weather?q=%s&APPID=23a8e5b6d7a5c5e91b7c93a703c196bb" % (loc)
@@ -16,6 +16,6 @@ def get_weather(loc):
 
 
 full = get_weather(city)
-
-print(full)
+m = full['main']
+print(m['humidity'])
 #time.sleep(interval)
