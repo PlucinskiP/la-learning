a08222fe2f0ba95b9f9c2642df457df8aed4c165
pawel <plucinski@gmx.com>
2019-05-21 20:27:31 +0200

    variables before docker

--- owm.py
+++ owm.py
@@ -4,13 +4,21 @@ import sys
 import time
 import os
 
+#city = sys.argv[1]
+#interval = int(sys.argv[2])
+os.system('echo Air humidity below 80%  >humidity.txt')
 
 
-city = sys.argv[1]
-interval = int(sys.argv[2])
-os.system('echo hello hello >humidity.txt')
-#print(city)
-#print(interval)
+FT = open("variable.txt","r")
+plik = (FT.read())
+x=plik.find(' ')
+city=plik[:x]
+x=x+1
+i=plik[x:]
+interval=int(i)
+FT.close()
+
+
 
 def get_weather(loc):
     url = "http://api.openweathermap.org/data/2.5/weather?q=%s&APPID=23a8e5b6d7a5c5e91b7c93a703c196bb" % (loc)
@@ -25,5 +33,3 @@ while(True):
     if h < 80:
         os.system("cat humidity.txt | wall")
     time.sleep(interval)
-
-
