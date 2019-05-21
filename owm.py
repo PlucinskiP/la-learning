9009aad9a3e52f67d4e3c2df1561ca6790ca242a
pawel <plucinski@gmx.com>
2019-05-21 22:51:38 +0200

    scrypt file

--- owm.py
+++ owm.py
@@ -1,24 +1,10 @@
-#!/home/pawel/PycharmProjects/openweathermap/venv/bin/python
 import requests
-#import sys
 import time
 import os
+import sys
 
-#city = sys.argv[1]
-#interval = int(sys.argv[2])
-os.system('echo Air humidity below 80%  >humidity.txt')
-
-
-FT = open("variable.txt","r")
-plik = (FT.read())
-x=plik.find(' ')
-city=plik[:x]
-x=x+1
-i=plik[x:]
-interval=int(i)
-FT.close()
-
-
+city = sys.argv[1]
+interval = int(sys.argv[2])
 
 def get_weather(loc):
     url = "http://api.openweathermap.org/data/2.5/weather?q=%s&APPID=23a8e5b6d7a5c5e91b7c93a703c196bb" % (loc)
@@ -27,9 +13,7 @@ def get_weather(loc):
 
 while(True):
     full = get_weather(city)
-    m = full['main']
-    h = (m['humidity'])
-    print(h)
-    if h < 80:
-        os.system("cat humidity.txt | wall")
+    if full['main']['humidity'] < 80:
+        print(full['main']['humidity'])
+        os.system("echo Air humidity below 80% | wall")
     time.sleep(interval)
